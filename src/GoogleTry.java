7095220ff0ab8d82289fa3966e7871e8fe416780
Carlos Sanchez Marin <casamatechs@gmail.com>
2016-02-09 20:35:25 +0100

    Added main

--- src/GoogleTry.java
+++ src/GoogleTry.java
@@ -13,9 +13,19 @@ public class GoogleTry {
 	}
 
 	public static void main(String[] args) {
-		GoogleTry t = new GoogleTry(10, 10);
 		try {
-			t.readFile("/home/carlos/prueba.txt");
+			BufferedReader br = new BufferedReader(new FileReader("/home/carlos/prueba.txt"));
+			String sCurrentLine = br.readLine();
+			String [] cr = sCurrentLine.split(" ");
+			int row = Integer.parseInt(cr[0]);
+			int column = Integer.parseInt(cr[1]);
+			GoogleTry imagen = new GoogleTry(row, column);
+			int i = 0;
+			while ((sCurrentLine = br.readLine()) != null) {
+				imagen.rellenar(sCurrentLine, i, imagen.getColumns());
+				i++;
+			}
+			br.close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -41,11 +51,20 @@ public class GoogleTry {
 
 	public void readFile(String path) throws IOException {
 		String sCurrentLine;
+		int i = 0;
 		BufferedReader br = new BufferedReader(new FileReader(path));
 		while ((sCurrentLine = br.readLine()) != null) {
-			System.out.println(sCurrentLine);
+			rellenar(sCurrentLine, i, getColumns());
+			i++;
 		}
 		br.close();
+	}
+
+	public void rellenar(String cadena, int fila, int columna){
+		for(int i = 0; i < columna ; i++){
+			if(cadena.charAt(i) == '#') this.painting[fila][i] = true;
+			else this.painting[fila][i] = false ;
+		}
 	}
 
 }
